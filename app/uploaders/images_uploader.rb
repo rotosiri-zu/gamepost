bea895c0e9cb57ee3f94b3f1016eeea9c598c496
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-02-04 19:22:45 +0900

    S3の設定

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -6,26 +6,13 @@ class ImagesUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development? || Rails.env.test? 
-    storage :file
-  else
+    # storage :file  
     storage :fog
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # 許可する画像の拡張子
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
-
-  # ファイル名を書き換える
-  def filename
-  "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.#{file.extension}" if original_filename
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
