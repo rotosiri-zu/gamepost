bdc140ae93e45b35566f9e70759b4cba5242c029
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-02-05 12:16:20 +0900

    追加

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -11,13 +11,21 @@ class ImagesUploader < CarrierWave::Uploader::Base
   else    
     storage :fog
   end
-  
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+  # ここでファイル形式を指定する
+  def filename
+    original_filename if original_filename
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
