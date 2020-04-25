94e38c23bff8da279142578531fe10edd44ba61e
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-25 23:18:18 +0900

    komito14

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  root to: 'games#index'
   resources :games do
     resources :reviews
     resources :videogames
@@ -11,6 +12,5 @@ Rails.application.routes.draw do
    get 'games/:id/destroy' => 'games#destroy'
    resources :users
    get 'users/users/:id' => 'users#listofposts'
-   root to: 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
